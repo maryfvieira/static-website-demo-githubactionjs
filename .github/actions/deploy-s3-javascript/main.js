e9e5304435cea882bebc658ad8ea998a337ec3af
Mary Felvie <mary.feliciano@gmail.com>
2025-08-14 15:39:51 -0300

    deploy feature

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,7 +4,7 @@ const exec = require('@actions/exec');
 
 function run() {
   // 1) Get some input values
-  const bucket = core.getInput('bu', { required: true });
+  const bucket = core.getInput('bucket', { required: true });
   const bucketRegion = core.getInput('bucket-region', { required: true });
   const distFolder = core.getInput('dist-folder', { required: true });
 
