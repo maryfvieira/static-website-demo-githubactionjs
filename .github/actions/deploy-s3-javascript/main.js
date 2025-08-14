3cd705e84d9cf9d0589e99ab1bcedf924eebe33f
Mary Felvie <mary.feliciano@gmail.com>
2025-08-14 15:36:28 -0300

    deploy feature

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,19 @@
 const core = require('@actions/core');
 // const github = require('@actions/github');
-// const exec = require('@actions/exec');
+const exec = require('@actions/exec');
 
-function run(){
-    core.notice('Starting deployment to AWS S3');
+function run() {
+  // 1) Get some input values
+  const bucket = core.getInput('bu', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  // 2) Upload files
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput('website-url', websiteUrl); // echo "website-url=..." >> $GITHUB_OUTPUT
 }
+
 run();
