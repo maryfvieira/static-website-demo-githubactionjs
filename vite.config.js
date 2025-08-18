2df994cd03f30c3479c35a71d603f4f74d3ee753
Mary Felvie <mary.feliciano@gmail.com>
2025-08-18 20:35:35 -0300

    ajuste para openid

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // importante para S3
   test: {
     globals: true,
     environment: 'jsdom',
